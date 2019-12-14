948fe1cebee6722062e68d72e2d168f8daa0058c
Tristan <tmocklercoding@gmail.com>
2019-12-14 13:41:50 -0600

    Add logout button

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,15 @@ import { Route, Switch} from 'react-router-dom';
 import VideoList from './components/videosList/VideoList';
 import Register from "./components/User/Register";
 import Login from "./components/User/Login";
+import Nav from "./components/Nav";
 import AuthRoute from "./util/route_util";
 const App = ()=>(
   <div>
+      <Nav path="/"/>
     <Switch>
       <AuthRoute path="/login" component={Login} routeType="auth"/>
-      <Route path="/register" component={Register} />
-      <Route path="/" component={VideoList} />
+      <AuthRoute path="/register" component={Register} routeType="auth"/>
+      {/* <Route path="/" component={VideoList} /> */}
     </Switch>
   </div>
 )
