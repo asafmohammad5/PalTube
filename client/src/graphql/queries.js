920bf4e7c8093d5cb094b25f94a8557d82237e0f
RyanGonzalezUSA <mhalim80@hotmail.com>
2019-12-16 10:56:26 -0800

    add searchbar & search results in frontend

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -1,34 +1,5 @@
 import gql from "graphql-tag";
 
-// export const FETCH_VIDEOS = gql`
-//   query filterVideos($criteria: String){
-//   videos(criteria: $criteria){
-//       _id
-//       title
-//       description
-//       category
-//       url
-//       keywords
-//       comments{
-//         _id
-//       }
-//     }
-//   }
-// `;
-
-// export const FETCH_VIDEO = gql`
-//   query queryVideo($id:ID!){
-//   video(_id:$id){
-//     _id
-//     title
-//     description
-//     category
-//     url
-//     keywords
-//   }
-// }
-// `;
-
 export default {
   IS_LOGGED_IN: gql`
     query IsUserLoggedIn {
