10abec0ee96a3d0ffc981acaa9687def1996935f
asaf <asafmohammad5@gmail.com>
2019-12-20 11:14:17 -0600

    Test 6

---
+++ client/build/precache-manifest.b30456c26f50cf76a51a672828fd4cf5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44895d1e5b38e4a55f707f88ea351f8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c861ca87a2c9bde2c224",
+    "url": "/static/js/2.38f43a3c.chunk.js"
+  },
+  {
+    "revision": "c9f28547140700d6e58a04fc429a8962",
+    "url": "/static/js/2.38f43a3c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16e93ada5b611c827e08",
+    "url": "/static/js/main.bd0bae4a.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
