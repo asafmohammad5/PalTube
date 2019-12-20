065a8f10ad7c6c449fe2b322624f5694336c3edb
RyanGonzalezUSA <mhalim80@hotmail.com>
2019-12-20 10:35:12 -0800

    fix search bar auth

--- client/src/components/Videos/VideoIndex.js
+++ client/src/components/Videos/VideoIndex.js
@@ -1,60 +1,132 @@
 import React, { Component } from 'react';
 import Queries from '../../graphql/queries';
-import { graphql } from 'react-apollo';
-import {Link} from 'react-router-dom';
+import { ApolloConsumer } from 'react-apollo';
+import { Link } from 'react-router-dom';
 import SideBar from '../ui/SideBar';
 import NavBar from '../NavBar';
-const {FETCH_VIDEOS} = Queries;
+const { FETCH_VIDEOS } = Queries;
 
 class VideoIndex extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: { videos: [] }, pageNumber: 0, perPage: 16,
+      loading: false, firstLoad: true, documentsFullyLoaded: false
+    }
+  }
+
   renderVideos() {
-    return this.props.data.videos.map((
+    if (this.state.loading) {
+      return <div
+        style={{
+          backgroundColor: '#1F1F1F', height: '100vh', width: '100wh', 'textAlign': 'center', margin: '0 auto',
+          'lineHeight': '100vh'
+        }} >loading...</div>
+    }
+    if (Object.values(this.state.data).length === 0) {
+      return null
+    }
+    return this.state.data.videos.map((
       { _id, title, url }) => {
       return (
         <div key={_id} className="video-container">
           <div>
             <object className="video-player">
-              <param name="movie" value={`${url}?modestbranding=1&amp;version=3&amp;hl=en_US;showinfo=0`}></param>
+              <param name="movie" value={`${url}?modestbranding=1&amp;version=3&amp;hl=en_US;showinfo=0&rel=0&modest=1`}></param>
               <param name="allowFullScreen" value="true"></param>
               <param name="allowscriptaccess" value="always"></param>
-              <embed src={`${url}?modestbranding=1&amp;version=3&amp;hl=en_US&amp;showinfo=0`}
+              <embed src={`${url}?modestbranding=1&amp;version=3&amp;hl=en_US&amp;showinfo=0&rel=0&modest=1`}
                 type="application/x-shockwave-flash"
                 className="video-player" allowscriptaccess="always" allowFullScreen={true}></embed>
-            </object>          
+            </object>
           </div>
           <div className="video-info">
             <Link to={`/videos/${_id}`}>
               <p className="video-title clickable">
-              {title}
+                {title}
               </p>
-              </Link>
+            </Link>
           </div>
         </div>
       );
     });
   }
 
-  render() {
-    if (this.props.data.loading || !this.props.data.videos) {
-      return null;
+  onVideosFetched = data => {
+    if (data.videos.length === 0) {
+      this.setState({ documentsFullyLoaded: true, loading: false });
+      return
     }
+    let newPageNumber = this.state.pageNumber += 1;
+    const videos = this.state.data.videos.concat(data.videos);
+    this.setState({ data: { videos: videos } });
+    this.setState({ pageNumber: newPageNumber });
+    this.setState({ loading: false, firstLoad: false });
+  }
+
+
+  componentDidMount() {
+    document.getElementById("btnLoad").click();
+    window.addEventListener('scroll', this.handleScroll, { passive: true });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    if (this.state.firstLoad || this.state.loading) {
+      return;
+    }
+    if (this.state.documentsFullyLoaded) {
+      window.removeEventListener('scroll', this.handleScroll)
+    }
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      document.getElementById("btnLoad").click();
+    }
+  }
+
+  render() {
     return (
-      <div>
-        <NavBar />
-        <div className="container">
-          <div className="flex-grid">
-              <SideBar/>
-            <section className="main">
-              <h3>Recommended Videos</h3>
-              <div className="row">
-                {this.renderVideos()}
+      <ApolloConsumer>
+        {client => (
+          <div id="scrollDiv">
+            <NavBar />
+            <div className="container">
+              <div className="flex-grid">
+                <SideBar />
+                <section className="main">
+                  <h3>Recommended Videos</h3>
+                  <div className="row">
+                    {this.state.data && this.renderVideos(this.state.data)}
+                  </div>
+                  <div style={{ width: '1px', height: '1px' }}>
+                    <button style={{ width: '1px', height: '1px' }} id="btnLoad" style={{ width: '100%', backgroundColor: 'darkgrey' }}
+                      onClick={async (e) => {
+                        e.preventDefault();
+                        this.setState({ loading: true });
+                        const { data } = await client.query({
+                          query: FETCH_VIDEOS,
+                          variables: { criteria: undefined, perPage: this.state.perPage, pageNumber: this.state.pageNumber },
+                          fetchPolicy: "network-only"
+                        });
+                        this.onVideosFetched(data);
+                      }}
+                    >
+                    </button>
+                  </div>
+                </section>
               </div>
-            </section>
+            </div>
           </div>
-        </div>
-      </div>
+        )}
+      </ApolloConsumer>
     );
   }
 }
 
-export default graphql(FETCH_VIDEOS)(VideoIndex);
+export default VideoIndex;
+
+// export default graphql(FETCH_VIDEOS, {
+//   options: (props) => { return { variables: { criteria: undefined, perPage: 0, pageNumber: 0 } } }
+// })(VideoIndex);
---
+++ client/src/components/Videos/__VideoIndex.js
@@ -0,0 +1,158 @@
+import React, { Component } from 'react';
+import Queries from '../../graphql/queries';
+import { compose, graphql, ApolloConsumer, useQuery, Query, withApollo } from 'react-apollo';
+import { Link } from 'react-router-dom';
+import SideBar from '../ui/SideBar';
+import NavBar from '../NavBar';
+import { merge } from 'lodash';
+const { FETCH_VIDEOS } = Queries;
+
+class VideoIndex extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { data: { videos: [] }, pageNumber: 0, perPage: 4 }
+    this.pageNumber = 0;
+  }
+
+  handleClick = async ()=> {
+    const { data } = await this.props.client.query({
+      query: FETCH_VIDEOS,
+      variables: { criteria: undefined, perPage: this.state.perPage, pageNumber: this.state.pageNumber },
+      fetchPolicy: "network-only"
+    })
+  }
+
+
+  renderVideos() {
+    if (Object.values(this.state.data).length === 0) {
+      return null
+    }
+    return this.state.data.videos.map((
+      { _id, title, url }) => {
+      return (
+        <div key={_id} className="video-container">
+          <div>
+            <object className="video-player">
+              <param name="movie" value={`${url}?modestbranding=1&amp;version=3&amp;hl=en_US;showinfo=0`}></param>
+              <param name="allowFullScreen" value="true"></param>
+              <param name="allowscriptaccess" value="always"></param>
+              <embed src={`${url}?modestbranding=1&amp;version=3&amp;hl=en_US&amp;showinfo=0`}
+                type="application/x-shockwave-flash"
+                className="video-player" allowscriptaccess="always" allowFullScreen={true}></embed>
+            </object>
+          </div>
+          <div className="video-info">
+            <Link to={`/videos/${_id}`}>
+              <p className="video-title clickable">
+                {title}
+              </p>
+            </Link>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  onVideosFetched = data => {
+    let newPageNumber = this.state.pageNumber += 1;
+    const videos = this.state.data.videos.concat(data.videos);
+    this.setState({ data: { videos: videos } });
+    this.setState({ pageNumber: newPageNumber });
+  }
+
+  updatePageNumber = () =>{
+    // let newPageNumber = this.state.pageNumber + 1
+    // this.setState({pageNumber: newPageNumber})
+    // debugger
+    this.pageNumber += 1
+  }
+
+  onFetchMore = (event) => {
+    const { data: { fetchMore } } = this.props;
+    fetchMore({
+      variables: { criteria: undefined, perPage: this.state.perPage, pageNumber: this.state.pageNumber },
+      notifyOnNetworkStatusChange: true,
+      updateQuery: (previousResult, { fetchMoreResult, queryVariables }) => {
+        // if (!fetchMoreResult) return previousResult;
+        // return Object.assign({}, previousResult, {
+        //   videos: [...previousResult.videos, ...fetchMoreResult.videos]
+        // });
+
+        this.onVideosFetched(fetchMoreResult)
+      },
+    });
+  }
+
+
+  render() {
+    return (
+      <Query query={FETCH_VIDEOS} fetchPolicy="cache"
+        notifyOnNetworkStatusChange = {true}
+      variables={{ criteria: undefined, perPage: this.state.perPage, pageNumber: this.pageNumber }}
+    >
+      {({ loading, error, data }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error</p>;
+        this.pageNumber += 1
+          // this.onVideosFetched(data)
+        // debugger
+        const videos = this.state.data.videos.map(video => {
+          return <div>
+            <div>{video.title} </div>
+            <br/>
+          </div>
+        })
+        return <div>
+          {videos}
+          <span style={{ width: '200px' }} onClick={(e) => { e.preventDefault(); this.onFetchMore() }}>load more</span>
+        </div>
+      }}
+    </Query>
+    )
+  }
+
+  Oldrender() {
+    return (
+      <ApolloConsumer>
+        {client => (
+          <div id="scrollDiv">
+            <NavBar />
+            <div className="container">
+              <div className="flex-grid">
+                <SideBar />
+                <section className="main">
+                  <h3>Recommended Videos</h3>
+                  <div className="row">
+                    {this.state.data && this.renderVideos(this.state.data)}
+                  </div>
+                  <div style={{ width: '100%' }}>
+                    <button style={{ width: '100%', backgroundColor: 'darkgrey' }}
+                      onClick={async () => {
+                        const { data } = await client.query({
+                          query: FETCH_VIDEOS,
+                          variables: { criteria: undefined, perPage: this.state.perPage, pageNumber: this.state.pageNumber },
+                          fetchPolicy: "network-only"
+                        });
+                        this.onVideosFetched(data);
+                      }}
+                    >
+                      Click me!
+                  </button>
+                  </div>
+                </section>
+              </div>
+            </div>
+          </div>
+        )}
+      </ApolloConsumer>
+    );
+  }
+}
+
+// export default VideoIndex;
+// export default withApollo(VideoIndex)
+// export default graphql(FETCH_VIDEOS)(VideoIndex)
+
+export default graphql(FETCH_VIDEOS, {
+  options: (props) => { return { variables: { criteria: undefined, perPage: 5, pageNumber: 0 } } }
+})(VideoIndex);
