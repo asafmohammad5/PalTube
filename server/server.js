5e1629ad91ebc03c633740c95b6af279f1acba79
asaf <asafmohammad5@gmail.com>
2019-12-19 18:09:41 -0600

    Fix bugs part 4

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ if (!db) {
 }
 
 if (process.env.NODE_ENV === 'production') {
-  // app.use(express.static('frontend/build'));
+  app.use(express.static('client/build'));
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, "..", 'client', 'build', 'index.html'));
   })
